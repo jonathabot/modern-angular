bb8e30d2a336ca7669b17456e32379d00c4bda98
jonathabot <lebotelhon@gmail.com>
2026-06-09 10:27:54 -0300

    component interaction + signal

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './hello.html',
   styleUrl: './hello.scss',
 })
-export class Hello {}
+export class Hello {
+  protected title = 'Welcome to Modern Angular!';
+
+  protected isDisabled = false;
+
+  protected onClick() {
+    console.log('Button clicked!');
+    this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  increaseCounter() {
+    this.count.update((value) => value + 1);
+  }
+
+  decreaseCounter() {
+    this.count.update((value) => value - 1);
+  }
+
+  resetCounter() {
+    this.count.set(0);
+  }
+}
